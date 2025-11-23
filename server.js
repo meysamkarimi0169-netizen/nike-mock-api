393426f36c958ef33340b345649d5f02be760740
meysamkarimi0169-netizen <meysam.karimi0169@gmail.com>
2025-11-23 18:01:20 +0330

    Update server.js

--- server.js
+++ server.js
@@ -213,10 +213,7 @@ app.get('/api/v1/comment/list', (req, res) => {
   const db = readData();
   const comments = db.comment || [];
   if (product_id) {
-    return res.json(comments.filter(c => c.id === product_id));
-  }
-  else{
-     return res.json('hi');
+    return res.json(comments.filter(c => c.id.toString() === product_id.toString()));
   }
   res.json(comments);
 });
